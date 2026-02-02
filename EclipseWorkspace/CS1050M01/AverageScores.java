ec4971031b74d072c04bddb26ca14b0a4e138d13
GreesonC <cgreeson22@gmail.com>
2026-02-02 13:44:12 -0700

    GradeCalculations

---
+++ EclipseWorkspace/CS1050M01/AverageScores.java
@@ -0,0 +1,19 @@
+/**
+ * Name: Cameron Greeson Class: 1050 (M/W) Description: Calculating finals
+ * grades based on the category weights
+ */
+public class AverageScores
+{
+	public static void main(String[] args)
+	{
+		int score1 = 80;
+		int score2 = 97;
+		int score3 = 85;
+		int total = score1 + score2 + score3;
+		double average = total / 3.0;
+		System.out.println("Total: " + total);
+		System.out.println("Average " + average);
+	}// end of main
+		// end of class
+		// TODO Auto-generated method stub
+}
---
+++ EclipseWorkspace/CS1050M01/GEM01GradeCalculations.java
@@ -0,0 +1,25 @@
+/**
+ * Name: Cameron Greeson Class: 1050 (M/W) Description: Calculating finals
+ * grades based on the category weights
+ */
+public class GEM01GradeCalculations
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		// Percentage Constants
+		final double CLASS_PART_WEIGH = .15;
+		final double CLASS_EXPLOR_WEIGH = .20;
+		final double CLASS_QUIZZES = .25;
+		final double PROJECTS = .20;
+		final double FINAL_DEMO = .20;
+		// Variable Grades
+
+		//
+	}
+
+}
