8350e2ab3997b9c9c421c78196d3cde429906b47
GreesonC <cgreeson22@gmail.com>
2026-02-06 18:43:05 -0700

    Update GEM01GradeCalculations.java

--- EclipseWorkspace/CS1050M01/GEM01GradeCalculations.java
+++ EclipseWorkspace/CS1050M01/GEM01GradeCalculations.java
@@ -1,5 +1,7 @@
-/**
- * Name: Cameron Greeson Class: 1050 (M/W) Description: Calculating finals
+/*
+ * Name: Cameron Greeson 
+ * Class: 1050 (M/W) 
+ * Description: Calculating finals
  * grades based on the category weights
  */
 public class GEM01GradeCalculations
@@ -18,8 +20,18 @@ public class GEM01GradeCalculations
 		final double PROJECTS = .20;
 		final double FINAL_DEMO = .20;
 		// Variable Grades
+		double classpart = (95.0);
+		double classexplor = (92.0);
+		double quizzes = (88.0);
+		double projects = (87.0);
+		double finaldemo = (93.0);
 
+		// grades * weight
+		System.out.print("Final Grade Calculations:");
+
+		System.out.println((classpart * CLASS_PART_WEIGH) + (classexplor * CLASS_EXPLOR_WEIGH)
+				+ (quizzes * CLASS_QUIZZES) + (projects * PROJECTS) + (finaldemo * FINAL_DEMO));
 		//
 	}
-
-}
+	// end of main
+}// end of class
