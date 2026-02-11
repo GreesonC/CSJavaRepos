53c90a64e5fa0cc4c4302c8ac95271b49ac57a7f
GreesonC <cgreeson22@gmail.com>
2026-02-11 13:41:20 -0700

    BodyMassIndex

--- EclipseWorkspace/CS1050M02/BodyMassIndexActivity.java
+++ EclipseWorkspace/CS1050M02/BodyMassIndexActivity.java
@@ -1,5 +1,6 @@
 /**
- * Name: Cameron Greeson Class: 1050 (M/W) Description: Calculating finals
+ * Name: Cameron Greeson Class: 1050 (M/W) 
+ * Description: Calculating finals
  * Calculate the BMI using formula in US units. User will enter weight in pounds
  * and height in inches
  */
@@ -29,20 +30,33 @@ public class BodyMassIndexActivity
 		Scanner keyboardInput = new Scanner(System.in);
 		// Tell the user what the program will do.
 		System.out.println("This program will calculate your " + "body mass index, or BMI.");
-
 		// Get the user's weight.
+
 		System.out.print("Enter your weight, in pounds: ");
 		// ADD CODE to input next double for weight
+		weight = keyboardInput.nextDouble();
 
 		// Get the user's height.
 		System.out.print("Enter your height, in inches: ");
 		// ADD CODE input next double for height
+		height = keyboardInput.nextDouble();
 
 		// Calculate the user's body mass index.
 		bmi = weight * FORM_BMI / (height * height);
-
-		// Display the user's BMI.
-		System.out.printf("Your body mass index (BMI) is %f", bmi);
-
-	}
-}
+		System.out.printf("Your body mass index (BMI) is %f.\n", bmi);
+		if (bmi >= 30)
+		{
+			System.out.println("You're Obese");
+		} else if (bmi >= 25)
+		{
+			System.out.println("You're Overweight");
+		} else if (bmi >= 18.5)
+		{
+			System.out.println("You're Normal Weight");
+		} else
+		{
+			System.out.println("Obese");
+		}
+		keyboardInput.close();
+	}// End of Main
+}// End of Class
