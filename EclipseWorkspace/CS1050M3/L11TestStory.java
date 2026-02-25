65d1a7b37a8a586cd408e39925b9eee493f79163
GreesonC <cgreeson22@gmail.com>
2026-02-25 13:46:54 -0700

    Loop tests & Test Story

---
+++ EclipseWorkspace/CS1050M3/L11TestStory.java
@@ -0,0 +1,35 @@
+
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+import java.util.Scanner;
+
+public class L11TestStory
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		Scanner userInput = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		String userName = "user";
+		String correctU = "correct";
+		String password = "checkP";
+		String correctP = "validP";
+
+		userName = userInput.next();
+		password = userInput.next();
+		if (userName.equals(correctU))
+			;
+
+		// System.out.println("Login Granted");
+
+	}
+
+}
--- EclipseWorkspace/CS1050M3/LoopTestPart2.java
+++ EclipseWorkspace/CS1050M3/LoopTestPart2.java
@@ -4,7 +4,7 @@
  */
 import java.util.Scanner;
 
-public class LoopTest
+public class LoopTestPart2
 {
 
 	/**
@@ -13,24 +13,29 @@ public class LoopTest
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		// PART 1
 		Scanner userInput = new Scanner(System.in);
-		// Add int
+		double base = (0);
 		int exponent = (0);
 		do
 		{
+			System.out.println("Enter a base (can be double)");
+			base = userInput.nextDouble();
 			System.out.println("Enter a positive number for exponent. ");
 			exponent = userInput.nextInt();
 			if (exponent <= 0)
 			{
-				System.out.println("Invalid answer. ");
+				System.out.println("Invalid answer. Enter a postive exponent.");
 			}
 		} while (exponent <= 0);
 		if (exponent > 0)
 			;
 		{
-			System.out.println("The exponent is: " + exponent);
-		}
+			System.out.println(base + " raised to the exponent " + exponent + " is ");
+			for (; exponent > 0;)
+				;
+			{
 
+			}
+		}
 	}
 }
