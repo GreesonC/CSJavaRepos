0922d9eb6a3305492529fede089a9191ff725aa3
GreesonC <cgreeson22@gmail.com>
2026-02-26 13:09:41 -0700

    Update LoopTestPart2.java

--- EclipseWorkspace/CS1050M3/LoopTestPart2.java
+++ EclipseWorkspace/CS1050M3/LoopTestPart2.java
@@ -14,8 +14,8 @@ public class LoopTestPart2
 	{
 		// TODO Auto-generated method stub
 		Scanner userInput = new Scanner(System.in);
-		double base = (0);
-		int exponent = (0);
+		double base = 0;
+		int exponent = 0;
 		do
 		{
 			System.out.println("Enter a base (can be double)");
@@ -27,15 +27,13 @@ public class LoopTestPart2
 				System.out.println("Invalid answer. Enter a postive exponent.");
 			}
 		} while (exponent <= 0);
-		if (exponent > 0)
+
+		System.out.println(base + " raised to the exponent " + exponent + " is ");
+		for (exponent = base; exponent > 0;)
 			;
 		{
-			System.out.println(base + " raised to the exponent " + exponent + " is ");
-			for (; exponent > 0;)
-				;
-			{
 
-			}
 		}
+
 	}
 }
