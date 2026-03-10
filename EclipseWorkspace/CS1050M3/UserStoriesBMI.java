1fe6b4b020eba34b4236d662e08c690ac211cbc7
GreesonC <cgreeson22@gmail.com>
2026-03-10 14:04:34 -0600

    UserStories updated

--- EclipseWorkspace/CS1050M3/UserStoriesBMI.java
+++ EclipseWorkspace/CS1050M3/UserStoriesBMI.java
@@ -1,10 +1,10 @@
-/**
- * 
- */
 
 /**
  * 
  */
+
+import java.util.Scanner;
+
 public class UserStoriesBMI
 {
 
@@ -14,7 +14,45 @@ public class UserStoriesBMI
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
+		// Summary
+		System.out.println("Entering your athletes height and weight to return their BMI on these BMI ranges.\r\n"
+				+ "Under 18.5: Underweight\r\n" + "18.5 to under 25: Normal\r\n" + "25 to under 30: Overweight\r\n"
+				+ "30 or greater: Obese");
+
+		// final double FORM_BMI = 703;
 
 	}
 
+	public static double validate(double currentHeight, double currentWeight)
+	{
+		double currentWeight = 0.0;
+
+		double currentHeight = 0.0;
+
+		double bmi = 0.0;
+
+		Scanner keyboardInput = new Scanner(System.in);
+		currentWeight = keyboardInput.nextDouble();
+		currentHeight = keyboardInput.nextDouble();
+
+		if (currentWeight <= 0.0 && currentHeight <= 0.0)
+			;
+		System.out.println("Invalid entry. Enter a postive double.");
+
+	}
+
+	// BMI calculation
+	public static double calculateBmi(double currentBmi)
+	{
+
+		currentBmi = weight * FORM_BMI / (height * height);
+
+	}
+
+	// Display athletes BMI value and category
+	public static void printSummary()
+	{
+
+	}
+	// loop back to add more athletes
 }
