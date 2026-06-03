1fc7d987f0858a192c6a463b3fb19f146e99a414
GreesonC <cgreeson22@gmail.com>
2026-06-03 11:37:05 -0600

    AbstractClasses

---
+++ EclipseWorkspace/CS2050/Dog.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+
+public class Dog extends Animal
+{
+
+	public Dog(String name)
+	{
+		super(name);
+	}
+
+	public void speak()
+	{
+		System.out.println("Bark!");
+	}
+}
---
+++ EclipseWorkspace/CS2050/InClassExercise01.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+public class InClassExercise01
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+
+		Dog dog = new Dog("Gryffin");
+
+		System.out.println(dog.getName());
+		dog.speak();
+	}
+}
