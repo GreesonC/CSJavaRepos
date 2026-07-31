25f35bd7f1788b58c364b8d36f392421e32239ae
GreesonC <cgreeson22@gmail.com>
2026-07-31 12:25:20 -0600

    LinkedQueue

--- EclipseWorkspace/Assignment4RideShare/LinkedQueue.java
+++ EclipseWorkspace/Assignment4RideShare/LinkedQueue.java
@@ -1,65 +1,96 @@
-public class LinkedQueue<T> {
-    
-	//TO COMPLETE: define two pointers front and rear
+public class LinkedQueue<T>
+{
+	private Node<T> front;
+	private Node<T> rear;
+	// TO COMPLETE: define two pointers front and rear
 
-    public void enqueue(T item) {
-    //TO COMPLETE    
-    }
+	public void enqueue(T item)
+	{
+		// TO COMPLETE
+		Node<T> newNode = new Node<>(item);
 
-    public T dequeue() {
-    //TO COMPLETE
-        return null;//Note: this is just a dummy return value.
-    }
+		if (isEmpty())
+		{
+			front = newNode;
+			rear = newNode;
+		} else
+		{
+			rear.setNextNode(newNode);
+			rear = newNode;
+		}
+	}
 
-    public T peek() {
-    //TO COMPLETE
-        return null;//Note: this is just a dummy return value.
-    }
+	public T dequeue()
+	{
+		// TO COMPLETE
+		if (isEmpty())
+		{
+			return null;
+		}
 
-    public boolean isEmpty() {
-    //TO COMPLETE
-    	return false;//Note: this is just a dummy return value.
-    }
+		T frontItem = front.getData();
+		front = front.getNextNode();
 
-    public void printQueue() {
-        
-    }
-    
-    private class Node <T>
-    {
-       private T data; // Entry in queue
-       private Node next; // Link to next node
-       
-       private Node(T dataPortion)
-       {
-          data = dataPortion;
-          next = null;
-       } // end constructor
-       
-       private Node(T dataPortion, Node linkPortion)
-       {
-          data = dataPortion;
-          next = linkPortion;
-       } // end constructor
-       
-       private T getData()
-       {
-          return data;
-       } // end getData
-       
-       private void setData(T newData)
-       {
-          data = newData;
-       } // end setData
-       
-       private Node getNextNode()
-       {
-          return next;
-       } // end getNextNode
-       
-       private void setNextNode(Node nextNode)
-       {
-          next = nextNode;
-       } // end setNextNode
-    } // end Node
+		if (front == null)
+		{
+			rear = null;
+		}
+
+		return frontItem;
+	}
+
+	public T peek()
+	{
+		// TO COMPLETE
+		return null;// Note: this is just a dummy return value.
+	}
+
+	public boolean isEmpty()
+	{
+		// TO COMPLETE
+		return false;// Note: this is just a dummy return value.
+	}
+
+	public void printQueue()
+	{
+
+	}
+
+	private class Node<T>
+	{
+		private T data; // Entry in queue
+		private Node next; // Link to next node
+
+		private Node(T dataPortion)
+		{
+			data = dataPortion;
+			next = null;
+		} // end constructor
+
+		private Node(T dataPortion, Node linkPortion)
+		{
+			data = dataPortion;
+			next = linkPortion;
+		} // end constructor
+
+		private T getData()
+		{
+			return data;
+		} // end getData
+
+		private void setData(T newData)
+		{
+			data = newData;
+		} // end setData
+
+		private Node getNextNode()
+		{
+			return next;
+		} // end getNextNode
+
+		private void setNextNode(Node nextNode)
+		{
+			next = nextNode;
+		} // end setNextNode
+	} // end Node
 }
--- EclipseWorkspace/Assignment4RideShare/RideRequest.java
+++ EclipseWorkspace/Assignment4RideShare/RideRequest.java
@@ -1,21 +1,57 @@
 
-import java.time.LocalDateTime;
 import java.time.Duration;
+import java.time.LocalDateTime;
 
-public class RideRequest {
-    //TO COMPLETE: define all attributes
+public class RideRequest
+{
+	// TO COMPLETE: define all attributes
+	private String passengerName;
+	private String pickupLocation;
+	private String destination;
+	private LocalDateTime requestTime;
+	private LocalDateTime assignedTime;
 
-    public RideRequest() {
-    //TO COMPLETE   
-    }
+	public RideRequest(String passengerName, String pickupLocation, String destination)
+	{
+		// TO COMPLETE
+		this.passengerName = passengerName;
+		this.pickupLocation = pickupLocation;
+		this.destination = destination;
+		requestTime = LocalDateTime.now();
+		assignedTime = null;
+	}
 
-    public void assignNow() {
-    //TO COMPLETE   
-    }
+	public void assignNow()
+	{
+		// TO COMPLETE
+		assignedTime = LocalDateTime.now();
+	}
 
-    public long getWaitTimeSeconds() {
-    //TO COMPLETE    
-        return 0; //Note: this is just a dummy return value.
-    }
+	public long getWaitTimeSeconds()
+	{
+		// TO COMPLETE
+		if (assignedTime == null)
+		{
+			return 0;
+		}
 
+		return Duration.between(requestTime, assignedTime).getSeconds();
+	}
+
+	@Override
+	public String toString()
+	{
+		String information = "Passenger: " + passengerName + "\nPickup Location: " + pickupLocation + "\nDestination: "
+				+ destination + "\nRequest Time: " + requestTime;
+
+		if (assignedTime == null)
+		{
+			information += "\nAssigned Time: Not assigned";
+		} else
+		{
+			information += "\nAssigned Time: " + assignedTime;
+		}
+
+		return information;
+	}
 }
